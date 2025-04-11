4e0a699fb27b4b17a92a5e3b733d1efc6f79be73
Zero-sama <lamtactvlqm@gmail.com>
2025-04-11 15:03:14 +0700

    Update: add fullstack (frontend + backend)

---
+++ src/main/java/com/web/demo/WebCameraApplication.java
@@ -0,0 +1,13 @@
+package com.web.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebCameraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebCameraApplication.class, args);
+	}
+
+}
